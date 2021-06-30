af9661fa8b284d77c25dcd9de3e3febccb6d3b45
gxy <xuyang.geng@gaea.com>
2021-06-30 10:25:58 +0800

    增加utils

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.scss'
+import util from 'Utils/index'
 
 interface IProps {
   name: string
@@ -8,9 +9,12 @@ interface IProps {
 
 function App(props: IProps) {
   const { name, age } = props
+  const sum: number = util.add(1, 2)
   return (
     <div className='app'>
-      <h1>{process.env.API_HOST}</h1>
+      <h1>
+        {process.env.API_HOST} {sum}
+      </h1>
       <span>{`Hello! I'm ${name}, ${age} yearssss old.`}</span>
     </div>
   )
